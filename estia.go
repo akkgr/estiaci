3faeccfbcbfa32d47cecc7f56d5b2eb984530e1f
Tasos K <Tasos K>
2016-01-07 19:52:32 +0200

    'apps'

--- estia.go
+++ estia.go
@@ -6,5 +6,18 @@ import (
 
 func init() {
 	router := NewRouter()
-	http.Handle("/", router)
+	http.Handle("/", corsHandler(router))
+}
+
+func corsHandler(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			w.WriteHeader(http.StatusOK)
+		} else {
+			h.ServeHTTP(w, r)
+		}
+	}
 }
